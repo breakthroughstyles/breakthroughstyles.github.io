c2770b7de026cb0ef1f5c548051944d0e67f7465
William Poindexter <wryanp@Williams-MacBook-Pro-2.local>
2017-10-08 17:13:36 -0400

    round 1!

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,16 @@ Version      : 1.0
 */
 (function($) {
 	'use strict';
-	
+
 	jQuery(document).on('ready', function(){
-	
+
 		/*PRELOADER JS*/
-		$(window).on('load', function() { 
+		$(window).on('load', function() {
 			$('.status').fadeOut();
-			$('.preloader').delay(350).fadeOut('slow'); 
-		}); 
-		/*END PRELOADER JS*/		
-		
+			$('.preloader').delay(350).fadeOut('slow');
+		});
+		/*END PRELOADER JS*/
+
 		/*START MENU JS*/
 			 if ($(window).scrollTop() > 200) {
               $('.fixed-top').addClass('menu-bg');
@@ -27,16 +27,16 @@ Version      : 1.0
 				} else {
 					$('.site-navigation, .header-white, .header').removeClass('navbar-fixed');
 				}
-			});		  
-		/*END MENU JS*/	
+			});
+		/*END MENU JS*/
 
-		/*START HOME SLIDER JS*/	
+		/*START HOME SLIDER JS*/
 		jQuery("#home-slider").flexslider({
 			directionNav: false,
 			controlnav: true,
 		});
-		/*END HOME SLIDER JS*/	
-		
+		/*END HOME SLIDER JS*/
+
 		/* START COUNTDOWN JS*/
 		$('.counter_feature').on('inview', function(event, visible, visiblePartX, visiblePartY) {
 			if (visible) {
@@ -53,9 +53,9 @@ Version      : 1.0
 				$(this).unbind('inview');
 			}
 		});
-		/* END COUNTDOWN JS */						
-		
-		/*START PACKAGE JS*/	
+		/* END COUNTDOWN JS */
+
+		/*START PACKAGE JS*/
 		$("#package-slider").owlCarousel({
 		   items:2,
 			itemsDesktop:[1000,3],
@@ -68,8 +68,8 @@ Version      : 1.0
 			autoPlay:false
 		});
 		/*END PACKAGE JS*/
-		
-		/*START DISCOUNT JS*/	
+
+		/*START DISCOUNT JS*/
 		$("#discount-slider").owlCarousel({
 		   items:3,
 			itemsDesktop:[1000,3],
@@ -82,8 +82,8 @@ Version      : 1.0
 			autoPlay:false
 		});
 		/*END DISCOUNT JS*/
-		
-		/*START GALLERY JS*/	
+
+		/*START GALLERY JS*/
 		$("#gallery-slider").owlCarousel({
 		   items:1,
 			itemsDesktop:[1000,3],
@@ -96,8 +96,8 @@ Version      : 1.0
 			autoPlay:false
 		});
 		/*END GALLERY JS*/
-		
-		/*START TESTIMONIAL JS*/	
+
+		/*START TESTIMONIAL JS*/
 		$("#testimonial-slider").owlCarousel({
 		   items:3,
 			itemsDesktop:[1000,3],
@@ -110,41 +110,41 @@ Version      : 1.0
 			autoPlay:false
 		});
 		/*END TESTIMONIAL JS*/
-		
+
 		/*START PARTNER LOGO*/
 		$('.partner').owlCarousel({
 		  autoPlay: 5000, //Set AutoPlay to 3 seconds
-		  items : 5,
+		  items : 2,
 		  itemsDesktop : [1199,3],
 		  itemsDesktopSmall : [979,3]
 		});
 		/*END PARTNER LOGO*/
-		
+
 		/*START GOOGLE MAP*/
 		function initialize() {
 		  var mapOptions = {
 			zoom: 15,
 			scrollwheel: false,
-			center: new google.maps.LatLng(-37.7622470161679, 145.06004333496094)
+			center: new google.maps.LatLng(33.834102, -84.008848)
 		  };
 		  var map = new google.maps.Map(document.getElementById('map'),
 			  mapOptions);
 		  var marker = new google.maps.Marker({
 			position: map.getCenter(),
-			icon: 'assets/img/map_pin.png',
+			icon: 'images/map_pin.png',
 			map: map
 		  });
 		}
-		google.maps.event.addDomListener(window, 'load', initialize);	
+		google.maps.event.addDomListener(window, 'load', initialize);
 		/*END GOOGLE MAP*/
-		
-	}); 	
-	
+
+	});
+
 	/* START PARALLAX JS */
 	(function () {
 
 		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		 
+
 		} else {
 			$(window).stellar({
 				horizontalScrolling: false,
@@ -153,14 +153,10 @@ Version      : 1.0
 		}
 
 	}());
-	/* END PARALLAX JS  */	
-	
+	/* END PARALLAX JS  */
+
 	/*START ANIMATION JS*/
 	  AOS.init();
-	/*END ANIMATION JS*/	
-				
+	/*END ANIMATION JS*/
+
 })(jQuery);
-
-
-  
-
